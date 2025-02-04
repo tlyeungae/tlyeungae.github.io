f33bfd3ecd350eed125e6f729479eecd9e402533
tlyeungae <tlyeungae@outlook.com>
2025-02-03 21:38:55 -0500

    fix bug

--- script.js
+++ script.js
@@ -9,27 +9,20 @@
     $('.main-menu a[href="'+ region +'"]').addClass('active');
   });
 
+  // Use just ONE document.ready block for the toggle logic
   $(document).ready(function() {
-    // 1) Existing or other code you might have
-
-    // 2) NEW: Abstract toggle code
     $('.toggle-abstract').on('click', function(e) {
       e.preventDefault();
-      // Toggle the .abstract-text that immediately follows the clicked link
-      $(this).next('.abstract-text').slideToggle();
-    });
-  });
 
-  $('.toggle-abstract').on('click', function(e) {
-    e.preventDefault();
-    const $abstractText = $(this).next('.abstract-text');
-    $abstractText.slideToggle();
-  
-    // Toggle link text
-    if ($abstractText.is(':visible')) {
-      $(this).text('Show Abstract');
-    } else {
-      $(this).text('Hide Abstract');
-    }
+      const $abstractText = $(this).next('.abstract-text');
+      $abstractText.slideToggle();
+
+      // Toggle link text after the slideToggle
+      if ($abstractText.is(':visible')) {
+        $(this).text('Hide Abstract');
+      } else {
+        $(this).text('Show Abstract');
+      }
+    });
   });
 })(jQuery);
